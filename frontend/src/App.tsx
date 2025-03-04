1b568c559967ec6ccf03a9fde122d33818148a11
nawzneen <earworm90@gmail.com>
2025-03-04 19:43:04 +0000

    Feat: add states and form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,83 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+interface promptType {
+  userPrompt: string;
+  systemPrompt: string;
+}
 
 function App() {
+  const [prompt, setPrompt] = React.useState<promptType>({
+    userPrompt: "",
+    systemPrompt: "",
+  });
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const handleUserPromptChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setPrompt({ ...prompt, userPrompt: e.target.value });
+  };
+  const handleSystemPromptChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setPrompt({ ...prompt, systemPrompt: e.target.value });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    // sending data to api
+    console.log(prompt);
+    setPrompt({ userPrompt: "", systemPrompt: "" });
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
       <div className="bg-gray-700 min-h-[100vh] flex flex-col align-center">
-        <div className="mt-16 ">
-          <p className="text-gray-100 font-bold text-3xl ">Ask a question:</p>
-          <div>
-            <textarea className="bg-white mt-8 w-[500px] h-[200px] rounded-md p-5 " />
-          </div>
-          <button className="w-[500px] bg-gray-400 py-2 rounded-md ">
-            Send
-          </button>
+        <div className="mt-16  flex  justify-center align-center">
+          <form
+            className=" flex flex-col justify-center align-center w-[80vw] "
+            onSubmit={handleSubmit}
+          >
+            <p className=" text-gray-100 font-bold text-3xl ">
+              Ask a question:
+            </p>
+            <div className="mt-8 flex flex-row gap-x-2 justify-center align-center w-[80vw]">
+              <div className="w-full">
+                <p className="py-2 bg-blue-200  rounded-t-md font-bold">
+                  User Prompt
+                </p>
+                <textarea
+                  className="bg-white  w-full h-[200px] rounded-b-md p-5 outline-none border-none  overflow-hidden  "
+                  value={prompt.userPrompt}
+                  onChange={handleUserPromptChange}
+                />
+              </div>
+              <div className="w-full">
+                <p className="py-2 bg-blue-200 rounded-t-md font-bold ">
+                  System Prompt
+                </p>
+                <textarea
+                  className="bg-white  w-full h-[200px] rounded-b-md p-5 outline-none border-none  overflow-hidden "
+                  value={prompt.systemPrompt}
+                  onChange={handleSystemPromptChange}
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className=" mt-2 bg-gray-400 py-2 rounded-md font-bold shadow-md "
+            >
+              Send
+            </button>
+          </form>
         </div>
         <div
           className=" justify-center align-center mt-16 grid grid-cols-3 gap-3
          px-10"
         >
           <div className="bg-gray-100 rounded-md ">
-            <p className="py-2">Model Name</p>
+            <p className="py-2 font-bold">Model Name</p>
             <hr />
             <p className="p-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -30,7 +87,7 @@ function App() {
             </p>
           </div>
           <div className="bg-gray-100  rounded-md">
-            <p className="py-2">Model Name</p>
+            <p className="py-2 font-bold">Model Name</p>
             <hr />
             <p className="p-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -44,7 +101,7 @@ function App() {
             </p>
           </div>
           <div className="bg-gray-100  rounded-md">
-            <p className="py-2">Model Name</p>
+            <p className="py-2 font-bold">Model Name</p>
             <hr />
             <p className="p-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
